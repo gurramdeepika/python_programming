f63cc9ed6733091ece773bcad43b2ab95c2ab40b
gurramdeepika <g.deepika649@gmail.com>
2018-11-23 10:13:01 +0530

    delete mysite

--- PycharmProjects/no1/mysite/myapp/book.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Book
-
-class ArticleForm(ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title','author','publisher','publication_date']
-fom = ArticleForm()
-print(fom)
